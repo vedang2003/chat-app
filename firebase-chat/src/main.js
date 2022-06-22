b970edef778c15e3832e550d1ca03f03ceff54c9
vedang2003 <vedangc03@gmail.com>
2022-06-22 20:22:28 +0530

    update

--- firebase-chat/src/main.js
+++ firebase-chat/src/main.js
@@ -1,4 +1,23 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import firebase from "firebase";
 
-createApp(App).mount('#app')
+var firebaseConfig = {
+    apiKey: "AIzaSyAW-Iiw-Ib7aK0sa3bK3mgk3ZimKJtRxVA",
+    authDomain: "firevuechat-42db2.firebaseapp.com",
+    databaseURL: "https://firevuechat-42db2-default-rtdb.firebaseio.com",
+    projectId: "firevuechat-42db2",
+    storageBucket: "firevuechat-42db2.appspot.com",
+    messagingSenderId: "456093947356",
+    appId: "1:456093947356:web:215e19db94a0c09049eb19"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount("#app");
