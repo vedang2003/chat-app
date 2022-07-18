c1f27991bfe680440715a5f04e1f8ef198d79fea
vedang2003 <vedangc03@gmail.com>
2022-07-18 18:47:51 +0530

    update

--- firebase-chat/src/components/HelloWorld.vue
+++ firebase-chat/src/components/HelloWorld.vue
@@ -1,138 +1,84 @@
 <template>
-    <div class="hello">
-        <h1>{{ msg }}</h1>
-        <p>
-            For a guide and recipes on how to configure / customize this
-            project,<br />
-            check out the
-            <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-                >vue-cli documentation</a
-            >.
-        </p>
-        <h3>Installed CLI Plugins</h3>
-        <ul>
-            <li>
-                <a
-                    href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-                    target="_blank"
-                    rel="noopener"
-                    >babel</a
-                >
-            </li>
-            <li>
-                <a
-                    href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-                    target="_blank"
-                    rel="noopener"
-                    >router</a
-                >
-            </li>
-            <li>
-                <a
-                    href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-                    target="_blank"
-                    rel="noopener"
-                    >eslint</a
-                >
-            </li>
-        </ul>
-        <h3>Essential Links</h3>
-        <ul>
-            <li>
-                <a href="https://vuejs.org" target="_blank" rel="noopener"
-                    >Core Docs</a
-                >
-            </li>
-            <li>
-                <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-                    >Forum</a
-                >
-            </li>
-            <li>
-                <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-                    >Community Chat</a
-                >
-            </li>
-            <li>
-                <a
-                    href="https://twitter.com/vuejs"
-                    target="_blank"
-                    rel="noopener"
-                    >Twitter</a
-                >
-            </li>
-            <li>
-                <a href="https://news.vuejs.org" target="_blank" rel="noopener"
-                    >News</a
-                >
-            </li>
-        </ul>
-        <h3>Ecosystem</h3>
-        <ul>
-            <li>
-                <a
-                    href="https://router.vuejs.org"
-                    target="_blank"
-                    rel="noopener"
-                    >vue-router</a
-                >
-            </li>
-            <li>
-                <a href="https://vuex.vuejs.org" target="_blank" rel="noopener"
-                    >vuex</a
-                >
-            </li>
-            <li>
-                <a
-                    href="https://github.com/vuejs/vue-devtools#vue-devtools"
-                    target="_blank"
-                    rel="noopener"
-                    >vue-devtools</a
-                >
-            </li>
-            <li>
-                <a
-                    href="https://vue-loader.vuejs.org"
-                    target="_blank"
-                    rel="noopener"
-                    >vue-loader</a
-                >
-            </li>
-            <li>
-                <a
-                    href="https://github.com/vuejs/awesome-vue"
-                    target="_blank"
-                    rel="noopener"
-                    >awesome-vue</a
-                >
-            </li>
-        </ul>
-    </div>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    
+    <h1>Is Initialized: {{ Vue3GoogleOauth.isInit }}</h1>
+    <h1>Is Authorized: {{ Vue3GoogleOauth.isAuthorized }}</h1>
+    <h2 v-if='user'>Logged in user: {{ user }}</h2>
+
+    <button @click='handleSignIn' :disabled='!Vue3GoogleOauth.isInit || Vue3GoogleOauth.isAuthorized'>Sign In</button>
+    <button @click='handleSignOut' :disabled='!Vue3GoogleOauth.isAuthorized'>Sign Out</button>
+  </div>
 </template>
 
 <script>
+import { inject } from 'vue';
+
 export default {
-    name: 'HelloWorld',
-    props: {
-        msg: String,
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+
+  data() {
+    return {
+      user: '',
+    }
+  },
+
+  methods: {
+    async handleSignIn() {
+      try {
+        const googleUser = await this.$gAuth.signIn();
+        // console.log(this.$gAuth.signIn);
+
+        if (!googleUser) {
+          return null;
+        }
+
+        this.user = googleUser.getBasicProfile().getEmail();
+      } catch (error) {
+        console.log(error);
+        return null;
+      }
+      
     },
-};
+    async handleSignOut() {
+      try {
+        await this.$gAuth.signOut();
+        // console.log(this.$gAuth.signOut);
+
+        this.user = '';
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  },
+
+  setup() {
+    const Vue3GoogleOauth = inject('Vue3GoogleOauth');
+
+    return {
+      Vue3GoogleOauth,
+    };
+  }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
-    margin: 40px 0 0;
+  margin: 40px 0 0;
 }
 ul {
-    list-style-type: none;
-    padding: 0;
+  list-style-type: none;
+  padding: 0;
 }
 li {
-    display: inline-block;
-    margin: 0 10px;
+  display: inline-block;
+  margin: 0 10px;
 }
 a {
-    color: #42b983;
+  color: #42b983;
 }
 </style>
